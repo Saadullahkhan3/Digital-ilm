e0f69489c30bc8a1d0358be24751822782f09ccc
Saadullah Khan <Saadullahkhan43251@gmail.com>
2024-10-02 01:58:46 +0500

    Version 1 of Digital ilm, Completed

--- Digitalilm/Digitalilm/settings.py
+++ Digitalilm/Digitalilm/settings.py
@@ -119,8 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-# Create new Static dirs
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
