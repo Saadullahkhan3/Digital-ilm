35306682a4218a595863fe938cade42aa9b3a34f
Saadullah Khan <Saadullahkhan43251@gmail.com>
2024-10-02 00:48:04 +0500

    Add explantion of each model and their relationship with each other, also mention more clearly in each model

--- Digitalilm/learn/models.py
+++ Digitalilm/learn/models.py
@@ -3,10 +3,30 @@ from django.contrib.auth.models import User
 
 
 
+# on_delete=models.CASCADE
+'''
+It deletes related things, such as
+When a User(Tutor) deletes its account then question sheets which are related to this Tutor will also be deleted and because question sheets are deleted then questions which are related to question sheet will also be deleted
+BUT it not happened in reverse order, means if any question is deleted, it not auto delete related question sheet and also when any question sheet deleted, it not also delete Tutor account.
+It works like a tree, where if root removed entire tree falldown but if any leaf dead, nothing happened to root.
+               User
+                |
+        -------------------
+        |                 |
+    QuestionSheet      QuestionSheet  
+        |                  |
+    -----------        -----------
+    |         |        |         |
+Question  Question     Question   Question
+'''
+
 # Create your models here.
 class QuestionSheet(models.Model):
     title = models.CharField(max_length=200)
 
+    # One to Many Relationship.
+    # where, User(built-in class) is one and QuestionSheet are many
+    # Means that one user can have multiple question sheet or multiple question sheet can have one tutor
     tutor = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tutor")
 
     # Ye ho,, ya na ho?
@@ -48,6 +68,9 @@ class Question(models.Model):
     c = models.CharField(max_length=100, blank=True, null=True)
     d = models.CharField(max_length=100, blank=True, null=True)
 
+    # One to Many Relationship.
+    # where, QuestionSheet is one and Question are many
+    # Means that one question sheet can have multiple question or multiple question sheet can have one question sheet
     question_sheet = models.ForeignKey(QuestionSheet, on_delete=models.CASCADE, related_name="questions")
 
     def __str__(self):
@@ -74,6 +97,10 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     score = models.IntegerField()
 
+    # One to Many Relationship.
+    # where, QuestionSheet is one and Student are many
+    # Means that one QuestionSheet can have multiple Students or multiple Students can have one question sheet
+    # Just like Question relationship with Questionsheet, swap question with student
     attempted_sheet = models.ForeignKey(QuestionSheet, on_delete=models.CASCADE, related_name="attempted_sheet")
 
 
