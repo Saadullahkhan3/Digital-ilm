83924c59c80b6e842f75294d0f48a80083346245
Saadullah Khan <Saadullahkhan43251@gmail.com>
2024-10-11 03:33:01 +0500

    un comment formset for edit sheet POST

--- Digitalilm/Digitalilm/settings.py
+++ Digitalilm/Digitalilm/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'learn',
+
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -118,7 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/' # below this add the following line
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
 # Default primary key field type
