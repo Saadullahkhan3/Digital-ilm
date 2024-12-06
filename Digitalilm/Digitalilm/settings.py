f4d47ad96723111e5394fc97660e2c1a2fb905c8
Saadullah Khan <Saadullahkhan43251@gmail.com>
2024-12-07 02:00:59 +0500

    In settings.py 'templates' to BASE_DIR / 'templates' as it is required for server settings

--- Digitalilm/Digitalilm/settings.py
+++ Digitalilm/Digitalilm/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'Digitalilm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
