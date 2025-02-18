d017ed99cdaf030ee81865f5fb3faa182720bd47
Saadullah Khan <Saadullahkhan43251@gmail.com>
2025-02-18 23:08:15 +0500

    Enabled in updated' auto_add_now=true

--- Digitalilm/learn/models.py
+++ Digitalilm/learn/models.py
@@ -38,8 +38,7 @@ class QuestionSheet(models.Model):
     # description = models.TextField(blank=True)
     
     created = models.DateTimeField(auto_now_add=True)
-    # updated = models.DateTimeField(auto_now=True)
-    updated = models.DateTimeField()
+    updated = models.DateTimeField(auto_now=True)
     
     # attempted_count = models.IntegerField(default=0)
 
